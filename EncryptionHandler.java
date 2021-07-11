d42aa2b8c2a91b15932bbd6fed264ad4a1f40e96
teluf <64613767+teluf@users.noreply.github.com>
2021-07-10 23:24:23 -0700

    Update EncryptionHandler.java

--- EncryptionHandler.java
+++ EncryptionHandler.java
@@ -7,147 +7,147 @@ public class EncryptionHandler {
 			char currentChar = x.charAt(i);
 			
 			switch(currentChar) {
-				case 'a':  
-					encryptedString = encryptedString + "{";  
-					break;  
-				    case 'b':  
-					encryptedString = encryptedString + "}";  
-					break;  
-				    case 'c':  
-					encryptedString = encryptedString + "#";  
-					break;  
-				    case 'd':  
-					encryptedString = encryptedString + "~";  
-					break;  
-				    case 'e':  
-					encryptedString = encryptedString + "+";  
-					break;  
-				    case 'f':  
-					encryptedString = encryptedString + "-";  
-					break;  
-				    case 'g':  
-					encryptedString = encryptedString + "*";  
-					break;  
-				    case 'h':  
-					encryptedString = encryptedString + "@";  
-					break;  
-				    case 'i':  
-					encryptedString = encryptedString + "/";  
-					break;  
-				    case 'j':  
-					encryptedString = encryptedString + "\\";  
-					break;  
-				    case 'k':  
-					encryptedString = encryptedString + "?";  
-					break;  
-				    case 'l':  
-					encryptedString = encryptedString + "$";  
-					break;  
-				    case 'm':  
-					encryptedString = encryptedString + "!";  
-					break;  
-				    case 'n':  
-					encryptedString = encryptedString + "^";  
-					break;  
-				    case 'o':  
-					encryptedString = encryptedString + "(";  
-					break;  
-				    case 'p':  
-					encryptedString = encryptedString + ")";  
-					break;  
-				    case 'q':  
-					encryptedString = encryptedString + "<";  
-					break;  
-				    case 'r':  
-					encryptedString = encryptedString + ">";  
-					break;  
-				    case 's' :  
-					encryptedString = encryptedString + "=";  
-					break;  
-				    case 't':  
-					encryptedString = encryptedString + ";";  
-					break;  
-				    case 'u':  
-					encryptedString = encryptedString + ",";  
-					break;  
-				    case 'v' :  
-					encryptedString = encryptedString + "_";  
-					break;  
-				    case 'w':  
-					encryptedString = encryptedString + "[";  
-					break;  
-				    case 'x' :  
-					encryptedString = encryptedString + "]";  
-					break;  
-				    case 'y':  
-					encryptedString = encryptedString + ":";  
-					break;  
-				    case 'z' :  
-					encryptedString = encryptedString + "\"";  
-					break;  
-				    case ' ' :  
-					encryptedString = encryptedString + " ";  
-					break;  
-				    case '.':  
-					encryptedString = encryptedString + '3';  
-					break;  
-				    case ',':  
-					encryptedString = encryptedString + "1";  
-					break;  
-				    case '(':  
-					encryptedString = encryptedString + '4';  
-					break;  
-				    case '\"':  
-					encryptedString = encryptedString + '5';  
-					break;  
-				    case ')' :  
-					encryptedString = encryptedString + "7";  
-					break;  
-				    case '?' :  
-					encryptedString = encryptedString + "2";  
-					break;  
-				    case '!':  
-					encryptedString = encryptedString + "8";  
-					break;  
-				    case '-' :  
-					encryptedString = encryptedString + "6";  
-					break;  
-				    case '%' :  
-					encryptedString = encryptedString + "9";  
-					break;  
-				    case '1':  
-					encryptedString = encryptedString + "r";  
-					break;  
-				    case '2':  
-					encryptedString = encryptedString + "k";  
-					break;  
-				    case '3':  
-					encryptedString = encryptedString + "b";  
-					break;  
-				    case '4':  
-					encryptedString = encryptedString + "e";  
-					break;  
-				    case '5':  
-					encryptedString = encryptedString + "q";  
-					break;  
-				    case '6':  
-					encryptedString = encryptedString + "h";  
-					break;  
-				    case '7':  
-					encryptedString = encryptedString + "u";  
-					break;  
-				    case '8' :  
-					encryptedString = encryptedString + "y";  
-					break;  
-				    case '9':  
-					encryptedString = encryptedString + "w";  
-					break;  
-				    case '0':  
-					encryptedString = encryptedString + "z";  
-					break;  
-				     default:  
-					encryptedString = encryptedString + "0";
-					break;  
+			    case 'a':  
+				encryptedString = encryptedString + "{";  
+				break;  
+			    case 'b':  
+				encryptedString = encryptedString + "}";  
+				break;  
+			    case 'c':  
+				encryptedString = encryptedString + "#";  
+				break;  
+			    case 'd':  
+				encryptedString = encryptedString + "~";  
+				break;  
+			    case 'e':  
+				encryptedString = encryptedString + "+";  
+				break;  
+			    case 'f':  
+				encryptedString = encryptedString + "-";  
+				break;  
+			    case 'g':  
+				encryptedString = encryptedString + "*";  
+				break;  
+			    case 'h':  
+				encryptedString = encryptedString + "@";  
+				break;  
+			    case 'i':  
+				encryptedString = encryptedString + "/";  
+				break;  
+			    case 'j':  
+				encryptedString = encryptedString + "\\";  
+				break;  
+			    case 'k':  
+				encryptedString = encryptedString + "?";  
+				break;  
+			    case 'l':  
+				encryptedString = encryptedString + "$";  
+				break;  
+			    case 'm':  
+				encryptedString = encryptedString + "!";  
+				break;  
+			    case 'n':  
+				encryptedString = encryptedString + "^";  
+				break;  
+			    case 'o':  
+				encryptedString = encryptedString + "(";  
+				break;  
+			    case 'p':  
+				encryptedString = encryptedString + ")";  
+				break;  
+			    case 'q':  
+				encryptedString = encryptedString + "<";  
+				break;  
+			    case 'r':  
+				encryptedString = encryptedString + ">";  
+				break;  
+			    case 's' :  
+				encryptedString = encryptedString + "=";  
+				break;  
+			    case 't':  
+				encryptedString = encryptedString + ";";  
+				break;  
+			    case 'u':  
+				encryptedString = encryptedString + ",";  
+				break;  
+			    case 'v' :  
+				encryptedString = encryptedString + "_";  
+				break;  
+			    case 'w':  
+				encryptedString = encryptedString + "[";  
+				break;  
+			    case 'x' :  
+				encryptedString = encryptedString + "]";  
+				break;  
+			    case 'y':  
+				encryptedString = encryptedString + ":";  
+				break;  
+			    case 'z' :  
+				encryptedString = encryptedString + "\"";  
+				break;  
+			    case ' ' :  
+				encryptedString = encryptedString + " ";  
+				break;  
+			    case '.':  
+				encryptedString = encryptedString + '3';  
+				break;  
+			    case ',':  
+				encryptedString = encryptedString + "1";  
+				break;  
+			    case '(':  
+				encryptedString = encryptedString + '4';  
+				break;  
+			    case '\"':  
+				encryptedString = encryptedString + '5';  
+				break;  
+			    case ')' :  
+				encryptedString = encryptedString + "7";  
+				break;  
+			    case '?' :  
+				encryptedString = encryptedString + "2";  
+				break;  
+			    case '!':  
+				encryptedString = encryptedString + "8";  
+				break;  
+			    case '-' :  
+				encryptedString = encryptedString + "6";  
+				break;  
+			    case '%' :  
+				encryptedString = encryptedString + "9";  
+				break;  
+			    case '1':  
+				encryptedString = encryptedString + "r";  
+				break;  
+			    case '2':  
+				encryptedString = encryptedString + "k";  
+				break;  
+			    case '3':  
+				encryptedString = encryptedString + "b";  
+				break;  
+			    case '4':  
+				encryptedString = encryptedString + "e";  
+				break;  
+			    case '5':  
+				encryptedString = encryptedString + "q";  
+				break;  
+			    case '6':  
+				encryptedString = encryptedString + "h";  
+				break;  
+			    case '7':  
+				encryptedString = encryptedString + "u";  
+				break;  
+			    case '8' :  
+				encryptedString = encryptedString + "y";  
+				break;  
+			    case '9':  
+				encryptedString = encryptedString + "w";  
+				break;  
+			    case '0':  
+				encryptedString = encryptedString + "z";  
+				break;  
+			     default:  
+				encryptedString = encryptedString + "0";
+				break;  
 			}
 		}
 		return encryptedString;
